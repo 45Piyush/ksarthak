d7e1cc58b2d578d4602bd72e67af9556d8a02acb
45Piyush <singh45piyush@gmail.com>
2024-03-17 12:57:35 +0530

    myapp: views,urls,templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello Django!!")
+def index_page(request):
+    return render(request,'index.html')
